89a091c0a52473866651a2040d696379b7464b08
naomitharrison <ntharrison@wpi.edu>
2019-11-23 12:31:07 -0500

    started list playlist labda function and added aws stuff to class path

--- src/main/ListPlaylistsHandler.java
+++ src/main/ListPlaylistsHandler.java
@@ -1,5 +1,37 @@
 package main;
 
-public class ListPlaylistsHandler {
+import java.util.List;
+
+import main.DB.PlaylistsDAO;
+import main.http.ListPlaylistResponse;
+import main.model.Playlist;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+
+public class ListPlaylistsHandler implements RequestHandler<Object,ListPlaylistResponse>{
+
+	public LambdaLogger logger;
+
+	List<Playlist> getPlaylists() throws Exception {
+		logger.log("in getPlaylists");
+		PlaylistsDAO dao = new PlaylistsDAO();
+		
+		return dao.getAllPlaylists();
+	}
+
+	@Override
+	public ListPlaylistResponse handleRequest(Object arg0, Context arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
