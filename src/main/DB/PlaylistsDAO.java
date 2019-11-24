b09e3fba5961797a3d24edaf645578687ef85533
Zackary Fitzgibbon <43519994+NinjaFitz204@users.noreply.github.com>
2019-11-24 00:10:17 -0500

    added database methods

--- src/main/DB/PlaylistsDAO.java
+++ src/main/DB/PlaylistsDAO.java
@@ -20,15 +20,25 @@ public class PlaylistsDAO {
     	}
     }
     
+    /**
+     * lists all videos in local library
+     * @return
+     * @throws Exception
+     */
     public List<VideoSegment> listAllVideos() throws Exception{
     	
     	try {
     		List<VideoSegment> videos = new ArrayList<VideoSegment>();
     		
+    		PreparedStatement ps = conn.prepareStatement("SELECT * FROM library");
+            ResultSet resultSet = ps.executeQuery();
     		
-    		
-    		
-    		
+            while (resultSet.next()) {
+        		VideoSegment vs = generateVideoSegment(resultSet);
+        		videos.add(vs);
+            }
+            resultSet.close();
+            ps.close();
     		
     		return videos;
     		
@@ -38,7 +48,72 @@ public class PlaylistsDAO {
         }
     	
     }
+    
+    /**
+     * lists all videos with specified title in local library
+     * @param title
+     * @return
+     * @throws Exception
+     */
+public List<VideoSegment> listAllVideosWithTitle(String title) throws Exception{
+    	
+    	try {
+    		List<VideoSegment> videos = new ArrayList<VideoSegment>();
+    		
+    		PreparedStatement ps = conn.prepareStatement("SELECT * FROM library where title = '" + title + "'");
+            ResultSet resultSet = ps.executeQuery();
+    		
+            while (resultSet.next()) {
+        		VideoSegment vs = generateVideoSegment(resultSet);
+        		videos.add(vs);
+            }
+            resultSet.close();
+            ps.close();
+    		
+    		return videos;
+    		
+    	} catch (Exception e) {
+        	e.printStackTrace();
+            throw new Exception("Failed in getting videos: " + e.getMessage());
+        }
+    	
+    }
+
+/**
+ * lists all videos by specified character in local library
+ * @param character
+ * @return
+ * @throws Exception
+ */
+public List<VideoSegment> listAllVideosByCharacter(String character) throws Exception{
 	
+	try {
+		List<VideoSegment> videos = new ArrayList<VideoSegment>();
+		
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM library where character = '" + character + "'");
+        ResultSet resultSet = ps.executeQuery();
+		
+        while (resultSet.next()) {
+    		VideoSegment vs = generateVideoSegment(resultSet);
+    		videos.add(vs);
+        }
+        resultSet.close();
+        ps.close();
+		
+		return videos;
+		
+	} catch (Exception e) {
+    	e.printStackTrace();
+        throw new Exception("Failed in getting videos: " + e.getMessage());
+    }
+	
+}
+	
+/**
+ * returns list of all the playlists
+ * @return
+ * @throws Exception
+ */
 	public List<Playlist> listAllPlaylists() throws Exception {
         
         try {
@@ -60,7 +135,7 @@ public class PlaylistsDAO {
             	
             	while (resultSet.next()) {
             		VideoSegment vs = generateVideoSegment(resultSet);
-            		playlist.add(vs);
+            		playlist.appendEntry(vs);
             	}
             	resultSet.close();
                 ps.close();
@@ -73,34 +148,71 @@ public class PlaylistsDAO {
         }
 	}
 
+	/**
+	 * creates video segment given resultset from playlist
+	 * @param resultSet
+	 * @return
+	 * @throws Exception
+	 */
 	private VideoSegment generateVideoSegment(ResultSet resultSet) throws Exception {
 		String URL = resultSet.getString("videoURL");
 		PreparedStatement ps = conn.prepareStatement("SELECT * FROM Library where URL = '" + URL + "'");
         ResultSet result = ps.executeQuery();
-        String title;
-        String character;
+        String title = "";
+        String character = "";
         
         while (resultSet.next()) {
         	title = result.getString("title");
         	character = result.getString("character");
         }
+        
+        result.close();
+        ps.close();
 		
 		return new VideoSegment(title,character,URL);
 	}
-
-	private Playlist generatePlaylist(ResultSet resultSet) {
+/**
+ * creats a playlist with empty video list
+ * @param resultSet
+ * @return
+ */
+	private Playlist generatePlaylist(ResultSet resultSet) throws Exception {
 		// TODO Auto-generated method stub
-		return new Playlist(resultSet.getString("playlistname"),new ArrayList<VideoSegments>());
+		return new Playlist(resultSet.getString("playlistname"));
 	}
 	
-	public Playlist getPlaylist(String playlistName) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * returns playlist with given name
+	 * @param playlistName
+	 * @return
+	 */
+	public Playlist getPlaylist(String playlistName) throws Exception {
+        try {
+        	Playlist playlist = new Playlist(playlistName);
+
+            
+        	PreparedStatement ps = conn.prepareStatement("SELECT * FROM playlists where playlistname = '" + playlist.getName() + "'");
+        	ResultSet resultSet = ps.executeQuery();
+            	
+            	while (resultSet.next()) {
+            		VideoSegment vs = generateVideoSegment(resultSet);
+            		playlist.appendEntry(vs);
+            	}
+            	resultSet.close();
+                ps.close();
+            
+            return playlist;
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+            throw new Exception("Failed in getting playlists: " + e.getMessage());
+        }
 	}
 
 	public List<VideoSegment> getPlaylistVideoSegments(String playlistName) {
-		// TODO Auto-generated method stub
-		return null;
+		Playlist p = getPlaylist(playlistName);
+		
+		return p.getVideos();
 	}
 
 
