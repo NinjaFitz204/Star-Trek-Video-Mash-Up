410339b41a72a3751d5b9f8fe0e3add6f1abcc4c
Teresa Saddler <tsaddler@wpi.edu>
2019-11-25 08:16:18 -0500

    Added JS functionality for GET playlists and videoSegments requests

--- js/get_videoSegments.js
+++ js/get_videoSegments.js
@@ -1,15 +1,36 @@
-videosURL = value;
-
-let request = new XMLHttpRequest();
-request.open('GET', videosURL, true);
-  request.onload = function () {
-  // Convert JSON data to an object
-  let videos = JSON.parse(this.response);
-  let output = '';
-  for (var i = 0; i < videos.length; i++) {    
-      output+= '<input type="checkbox" name=' + videos[i].id + '>' + '<br>' + videos[i].name + '<br>' + videos[i].character
-    output += '<li>' +  + ' is ' +  + ' years old.'; '</li>'
-  }
-  document.getElementById('videos').innerHTML = output;
+function refreshVideoSegments() {
+	let request = new XMLHttpRequest();
+	request.open('GET', videos_url, true);
+	request.send();
+	
+	console.log("sent");
+	
+	request.onload = function() {
+		if (request.readyState == XMLHttpRequest.DONE) {
+			console.log ("request:" + request.responseText);
+			processVideoListResponse(request.responseText);
+		} else {
+			processVideoListResponse("N/A");
+		}
+	};
 }
-request.send();
+
+function processVideoListResponse(result) {
+	console.log("res:" + result);
+	var js = JSON.parse(result);
+	var videoList = document.getElementById('videoSegmentList');
+
+	var output = '';
+	output +='<ul style="list-style-type:none;">';
+	for (var i = 0; i < js.listOfSegments.length; i++) {
+		var constantJson = js.listOfSegments[i];
+		console.log(constantJson);
+
+		var ctitle = constantJson["title"];
+		var ccharacter = constantJson["character"];
+		var curl = constantJson["url"];
+		output += '<li><input type="checkbox" name="' + curl + '"><br> Line:' + ctitle + '<br> Character: ' + ccharacter + '</li>';
+	}
+	output += '</ul>';
+	videoList.innerHTML = output;
+}
